b4254a9ebee19db65a57a652802972f654921258
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-08-25 00:25:43 +0200

    minor changes

--- config/base.js
+++ config/base.js
@@ -3,9 +3,12 @@ const path = require("path");
 const CleanPlugin = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CheckerPlugin} = require("awesome-typescript-loader");
+const ManifestPlugin = require("webpack-manifest-plugin");
 const StyleLintPlugin = require("stylelint-webpack-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const {CheckerPlugin} = require("awesome-typescript-loader");
 
 module.exports = function (env) {
     return {
@@ -39,7 +42,7 @@ module.exports = function (env) {
                 from: "src/index.html"
             }, {
                 from: "src/manifest.json"
-            },  {
+            }, {
                 from: "src/assets/i18n", to: "assets/i18n"
             }, {
                 from: "src/assets/imgs", to: "assets/imgs"
@@ -68,6 +71,46 @@ module.exports = function (env) {
                 }
             }),
 
+            // Speed up build caching node_modules
+            // new HardSourceWebpackPlugin(),
+
+            // Generate a manifest file which contains a mapping of all asset filenames
+            // to their corresponding output file so that tools can pick it up without
+            // having to parse `index.html`.
+            new ManifestPlugin({
+                fileName: "asset-manifest.json"
+            }),
+            // Generate a service worker script that will precache, and keep up to date,
+            // the HTML & assets that are part of the Webpack build.
+            new SWPrecacheWebpackPlugin({
+                // By default, a cache-busting query parameter is appended to requests
+                // used to populate the caches, to ensure the responses are fresh.
+                // If a URL is already hashed by Webpack, then there is no concern
+                // about it being stale, and the cache-busting can be skipped.
+                dontCacheBustUrlsMatching: /\.\w{8}\./,
+                filename: "service-worker.js",
+                logger(message) {
+                    if (message.indexOf("Total precache size is") === 0) {
+                        // This message occurs for every build and is a bit too noisy.
+                        return;
+                    }
+                    if (message.indexOf("Skipping static resource") === 0) {
+                        // This message obscures real errors so we ignore it.
+                        // https://github.com/facebookincubator/create-react-app/issues/2612
+                        return;
+                    }
+                    console.log(message);
+                },
+                minify: true,
+                // For unknown URLs, fallback to the index page
+                navigateFallback: path.resolve(__dirname, "../dist") + "/index.html",
+                // Ignores URLs starting from /__ (useful for Firebase):
+                // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+                navigateFallbackWhitelist: [/^(?!\/__).*/],
+                // Don't precache sourcemaps (they're large) and build asset manifest:
+                staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+            }),
+
             new StyleLintPlugin()
         ],
 
@@ -87,8 +130,8 @@ module.exports = function (env) {
                             {loader: "typings-for-css-modules-loader", options: {camelCase: true, modules: true, minimize: true, namedExport: true}},
                             {loader: "resolve-url-loader"},
                             {loader: "sass-loader", options: {sourceMap: true}},
-                            // make base classes / variables available everywhere
-                            {loader: 'sass-resources-loader', options: {resources: "./src/assets/stylesheets/base.scss"}}
+                            {loader: "sass-resources-loader", options: {resources: "./src/assets/stylesheets/base.scss"}
+                            }
                         ]
                     })
                 },
--- src/app/navigation-bar/navigation-bar.ts
+++ src/app/navigation-bar/navigation-bar.ts
@@ -1,4 +1,4 @@
-import * as styles from "./navigation-bar.scss";
+// import * as styles from "./navigation-bar.scss";
 
 export default class NavigationBarComponent {
     public el: HTMLElement;
@@ -11,17 +11,17 @@ export default class NavigationBarComponent {
     protected render(): void {
         // Create external nav
         this.el = document.createElement("nav");
-        this.el.classList.add(styles.navigationBarComponent,
+        this.el.classList.add(/*styles.navigationBarComponent,*/
             "navbar", "navbar-toggleable-md", "navbar-inverse", "bg-primary", "fixed-top");
 
         // Items
         const itemList = document.createElement("ul");
         itemList.classList.add("navbar-nav", "mr-auto");
 
-        const navItem  = document.createElement("li");
+        const navItem = document.createElement("li");
         navItem.classList.add("nav-item");
 
-        const navLink  = document.createElement("a");
+        const navLink = document.createElement("a");
         navLink.classList.add("nav-link");
         navLink.appendChild(document.createTextNode("d3.js demo"));
 
