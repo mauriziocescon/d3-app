81920ae9bc577d1897aa3018fe2ae2c3ac092490
mau <maurizio.cescon@gmail.com>
2017-11-16 14:56:29 +0100

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  // json-server mock server
-  apiUrl: "http://localhost:5000/api/",
+    // json-server mock server
+    apiUrl: "http://localhost:5000/api/",
+    production: false,
 };
