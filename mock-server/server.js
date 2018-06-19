df1a2001b2a4fd06e06c4b7e302c7d0a751cac6a
mau <maurizio.cescon@gmail.com>
2018-06-19 10:41:15 +0200

    deps update

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const jsonServer = require("json-server");
+const path = require('path');
+const jsonServer = require('json-server');
 const app = jsonServer.create();
-const dbUrl = require("./db/constants").dbUrl;
+const dbUrl = require('./db/constants').dbUrl;
 const router = jsonServer.router(dbUrl);
 const middlewares = jsonServer.defaults({
-  static: "dist",
+  static: 'dist',
 });
 
-const delayMiddleware = require("./middlewares/delay");
-const errosMiddleware = require("./middlewares/errors");
+const delayMiddleware = require('./middlewares/delay');
+const errosMiddleware = require('./middlewares/errors');
 
 // set the port of our application
-// process.env.PORT lets the port be set by Heroku
+// process.env.PORT lets the port to be set by Heroku
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -20,12 +20,12 @@ app.use(delayMiddleware.delay);
 app.use(errosMiddleware.error);
 
 // Mount the router based on db.json
-app.use("/api", router);
+app.use('/api', router);
 
 // Fallback on frontend routes
-app.get("*", (req, res) => {
-  // load the single view file (frontend will handle the page changes on the front-end)
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+app.get('*', (req, res) => {
+  // load index.html (frontend will handle page changes)
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // Start listening
