02280b22f3062edd24c56c4bf7dc88a885153568
Maurizio Cescon <maurizio.cescon@gmail.com>
2023-11-12 00:00:27 +0100

    chore: deps

--- config/base.js
+++ config/base.js
@@ -84,7 +84,7 @@ module.exports = (env) => {
           test: /\.(js|ts|tsx)?$/,
           exclude: [/node_modules/],
           use: [
-            {loader: 'babel-loader', options: {cacheDirectory: true, presets: ['@babel/env']}},
+            {loader: 'babel-loader', options: {cacheDirectory: true, babelrc: true}},
             {loader: 'ts-loader'},
           ],
         },
